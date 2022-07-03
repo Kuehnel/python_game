e882abde1020cd4d0c6666cc377299d5f780cb20
Nora <kuehnel.nora@gmail.com>
2022-07-03 12:53:09 +0200

    change display caption

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame
 
 # pygame init + conf
 pygame.init()
-pygame.display.set_caption("Test")
+pygame.display.set_caption("Pinky vs. Pirates")
 screen_width = 1800
 screen_height = 900
 screen = pygame.display.set_mode([screen_width, screen_height])
